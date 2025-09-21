ed8f974bb2e4795a396c6e85330e03091aac9d74
Uday Jamgade <udayjamgade1@gmail.com>
2025-09-22 00:13:36 +0530

    deploy-2

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -61,7 +61,7 @@ const Login = ({ setLogin }) => {
             type="submit"
             className="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition duration-300"
           >
-            Login
+             Login
           </button>
         </form>
       </div>
